e4bdb468ce91fdea57f609245e80c40c4b9e4d51
Phan Thanh Tri <titp269@gmail.com>
2023-04-09 18:40:07 +0700

    JavaScript F8 - L1 L2

---
+++ Lesson_1/main.js
@@ -0,0 +1,15 @@
+
+var fullName = 'Phan Thanh Tri';
+var age = 20;
+
+console.log(age);
+console.log(fullName);
+console.warn("Nguy hiểm");
+console.error("Hư ròi!!");
+
+confirm("MAY DU TUOI CHUA NHOC?");
+prompt("Xác nhận độ tuổi và nhập tuổi của bạn: ");
+
+setTimeout(function() {
+    alert("PT26");
+},3000)
