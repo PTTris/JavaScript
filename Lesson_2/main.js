2395ecde38cacd15a1213e5b52fed6f890692c2a
Phan Thanh Tri <titp269@gmail.com>
2023-04-18 09:47:39 +0700

    Push Lesson 3

--- Lesson_2/main.js
+++ Lesson_2/main.js
@@ -8,3 +8,15 @@ var myArr = ['JS', 'HTML', 'CSS'];
 
 console.log (myObject);
 console.log (myArr);
+
+//Arguments
+
+function writeLog() {
+    var myString = ''; //Để nhận biết này là chuỗi
+    for (var param of arguments){
+        myString += `${param} - `;
+    }
+    console.log(myString);
+}
+
+writeLog('Tri', 'TuKhanh', 'VoChong');
